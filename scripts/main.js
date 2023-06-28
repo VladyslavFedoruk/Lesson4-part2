63f790ee335b6987f9ab34341d7db1fa25b07737
“Vladyslav.Fedoruk” <mr.fedoruk17@gmail.com>
2023-06-28 23:12:19 +0300

    some changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
 //Вивести на сторінку в один рядок через кому числа від 10 до 20.
 document.write(`- Вивести на сторінку в один рядок через кому числа від 10 до 20: <br>`)
-for(let i=10; i<=20; i++) 
-document.write(`${i} <br>`);
+for (let i = 10; i <= 20; i++)
+  document.write(`${i} <br>`);
 
 //Вивести квадрати чисел від 10 до 20.
 document.write(`- Вивести квадрати чисел від 10 до 20: <br>`)
 for (let i = 10; i <= 20; i++) {
-  let square = i **2;
+  let square = i ** 2;
   document.write(`${square} <br>`);
 }
 
@@ -20,46 +20,46 @@ for (let i = 1; i <= 10; i++) {
 //Знайти суму всіх цілих чисел від 1 до 15.
 document.write(`- Знайти суму всіх цілих чисел від 1 до 15: <br>`)
 let sum = 0;
-for (let i = 1; i <= 15; i++){
-    sum += i;
-} 
+for (let i = 1; i <= 15; i++) {
+  sum += i;
+}
 document.write(`Сума чисел: ${sum} <br>`)
 
 
 //Знайти добуток усіх цілих чисел від 15 до 35
 document.write(`- Знайти добуток усіх цілих чисел від 15 до 35: <br>`);
 let dobytok = 1;
-for (let i = 15; i <= 35; i++){
-    dobytok *= i;
-} 
+for (let i = 15; i <= 35; i++) {
+  dobytok *= i;
+}
 document.write(`Добуток чисел: ${dobytok} <br>`);
 
 //Знайти середнє арифметичне всіх цілих чисел від 1 до 500
 document.write(`- Знайти середнє арифметичне всіх цілих чисел від 1 до 500: <br>`);
 let counter = 0;
-for (let i = 1; i <= 500; i++){
-    counter += i;
-} 
+for (let i = 1; i <= 500; i++) {
+  counter += i;
+}
 let average = counter / 500
 document.write(`Cереднє арифметичне: ${average} <br>`);
 
 //Вивести суму лише парних чисел в діапазоні від 30 до 80
 document.write(`- Вивести суму лише парних чисел в діапазоні від 30 до 80: <br>`);
 let couple = 0;
-for (let i = 30; i <= 80; i++){
-    if (i % 2 === 0) {
-        couple += i;
-} 
-   }
+for (let i = 30; i <= 80; i++) {
+  if (i % 2 === 0) {
+    couple += i;
+  }
+}
 document.write(`Сума парних чисел в діапазоні від 30 до 80: ${couple} <br>`);
 
 //Вивести всі числа в діапазоні від 100 до 200 кратні 3.
 document.write(`- Вивести всі числа в діапазоні від 100 до 200 кратні 3: <br>`);
-for (let k = 100; k <= 200; k++){
-    if (k % 3 === 0) {
-        document.write(`${k} <br>`);
-} 
-   }
+for (let k = 100; k <= 200; k++) {
+  if (k % 3 === 0) {
+    document.write(`${k} <br>`);
+  }
+}
 
 //Дано натуральне число. Знайти та вивести на сторінку всі його дільники.
 document.write(`- Дано натуральне число. Знайти та вивести на сторінку всі його дільники: <br>`);
@@ -71,30 +71,30 @@ for (var i = 1; i <= number; i++) {
   }
 }
 // Визначити кількість його парних дільників
-let Count = 0;
+let count = 0;
 for (let i = 1; i <= number; i++) {
   if (number % i === 0 && i % 2 === 0) {
-    Count++;
+    count++;
   }
 }
-document.write("<br>Кількість парних дільників: " + Count);
+document.write("<br>Кількість парних дільників: " + count);
 
 // Знайти суму його парних дільників
-let CountSum = 0;
+let countSum = 0;
 for (var i = 1; i <= number; i++) {
   if (number % i === 0 && i % 2 === 0) {
-    CountSum += i;
+    countSum += i;
   }
 }
-document.write("<br>Сума парних дільників: " + CountSum);
+document.write("<br>Сума парних дільників: " + countSum);
 
 //Надрукувати повну таблицю множення від 1 до 10
 document.write(`- Надрукувати повну таблицю множення від 1 до 10: <br>`);
-for (let i = 1; i <= 10; i++){
-      document.write(`Таблиця множення на ${i}:<br>`);
- for (let s = 1; s <= 10; s++) {
-     let table_result = i * s;
+for (let i = 1; i <= 10; i++) {
+  document.write(`Таблиця множення на ${i}:<br>`);
+  for (let s = 1; s <= 10; s++) {
+    let table_result = i * s;
     document.write(`${i} * ${s} = ${table_result}<br>`);
-} 
+  }
   document.write("<br>");
 }
